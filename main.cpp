ab667bc4a74fa5056d86ccc27be4550bf5b780bf
flouder6110 <sgarce23sp@ollusa.edu>
2025-05-24 11:23:57 +0900

    main.cpp

---
+++ main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <cstring>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+
+	int i = 1; // for loop start at 1.
+	char digit;
+	int oddCounter = 0;
+	int evenCounter = 0;
+
+
+	cout << "Enter a number with even amount of digits: ";
+	digit = cin.get();
+
+	while (digit != 10)
+	{
+		int doubled = 2 * (digit - '0');
+		int num = (digit - '0');
+
+		if (i % 2 == 0)
+		{
+			if (doubled < 10)
+			{
+
+				oddCounter += doubled;
+				evenCounter += num;
+
+			}
+			else
+			{
+				oddCounter += (doubled / 10) + (doubled % 10);
+				evenCounter += num;
+			}
+		}
+		else {
+
+			if (doubled < 10)
+			{
+				evenCounter += doubled;
+				oddCounter += num;
+
+			}
+			else
+			{
+				evenCounter += (doubled / 10) + (doubled % 10);
+				oddCounter += num;
+			}
+		}
+		digit = cin.get();
+		i++;
+	}
+
+	int checkSum;
+
+	if ((i - 1) % 2 == 0) checkSum = evenCounter;
+	else checkSum = oddCounter;
+
+	cout << "The checkSum is " << checkSum << endl;
+
+	if (checkSum % 10 == 0)
+	{
+		cout << checkSum << " Is a Valid Number" << endl;
+	}
+	else
+	{
+		cout << checkSum << " Is NOT a Valid Number" << endl;
+	}
+
+	return 0;
+}
